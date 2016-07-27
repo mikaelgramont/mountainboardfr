ed1cd5deb7ee307806fe390b48878c3a672cb65c
Mikael Gramont <mgramont@gmail.com>
2016-07-27 12:16:25 +0200

    Removed the user_photo_video_tags permission. It's only been deprecated for 5 years. No big deal.

--- library/Lib/View/Helper/Facebook/UploadLink.php
+++ library/Lib/View/Helper/Facebook/UploadLink.php
@@ -5,7 +5,7 @@ class Lib_View_Helper_Facebook_UploadLink extends Zend_View_Helper_Abstract
 	{
 		$uploadUrl = APP_URL.$this->view->url(array(),'uploadphotomain');
 		$appId = FACEBOOK_APP_ID;
-		$facebookUploadAuthUrl = "https://www.facebook.com/dialog/oauth?client_id=$appId&scope=user_photos,user_photo_video_tags&response_type=token&redirect_uri=".urlencode($uploadUrl);
+		$facebookUploadAuthUrl = "https://www.facebook.com/dialog/oauth?client_id=$appId&scope=user_photos&response_type=token&redirect_uri=".urlencode($uploadUrl);
 
 		$return = '	<div class="addPhotoFacebook"><a class="dataLink facebook" href="'.$facebookUploadAuthUrl.'">'.ucfirst($this->view->translate('uploadFacebookPhoto')).'</a></div>'.PHP_EOL;
 		return $return;
