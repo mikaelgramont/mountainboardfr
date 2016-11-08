04c45cc4955410557255b68b90803175451e27a2
Mikael Gramont <mgramont@gmail.com>
2016-11-08 23:20:18 +0100

    Updating search log table definition to deal with ipv6

--- application/models/Search/Log.php
+++ application/models/Search/Log.php
@@ -9,7 +9,7 @@ CREATE TABLE `searches` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL,
   `userId` int(11) NOT NULL,
-  `ip` int(11) NOT NULL,
+  `ip` int(39) NOT NULL,
   `hostname` varchar(256) NOT NULL,
   `sessionId` varchar(32)  NOT NULL,
   `searchTerm` varchar(512) DEFAULT NULL,
