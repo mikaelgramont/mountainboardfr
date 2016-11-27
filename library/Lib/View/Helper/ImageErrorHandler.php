126a603830a2e7941390c656381128278e4854ff
Mikael Gramont <mgramont@gmail.com>
2016-11-27 14:06:24 +0100

    Moving error handler higher in the page so it catches errors before jQuer wakes up.

--- library/Lib/View/Helper/ImageErrorHandler.php
+++ library/Lib/View/Helper/ImageErrorHandler.php
@@ -5,18 +5,19 @@ class Lib_View_Helper_ImageErrorHandler extends Zend_View_Helper_Abstract
 	 * Hides images that error out.
 	 * Ideally, we'd log them, but hey, that's what server logs are for.
 	 */
-	public function imageErrorHandler()
+	public function imageErrorHandler($cspNonce)
 	{
 		$log = LOG_IMG_ERRORS ? "console.log('Image error', e.target.src, e);" : "";
 
 		$js = "
+<script nonce=\"$cspNonce\">
 document.body.addEventListener('error', function(e) {
 	${log}
 	if (e.target.tagName == 'IMG') {
 		e.target.style = 'display:none';
 	}
-}, true);";
-    $this->view->jQuery()->addOnLoad($js);
-
+}, true);
+</script>";
+		return $js;
 	}
 }
