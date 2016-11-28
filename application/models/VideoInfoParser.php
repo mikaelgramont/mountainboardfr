9408c2a2ea241bbb7336bdd3fe19ab0cb65ead3b
Mikael Gramont <mgramont@gmail.com>
2016-11-28 08:08:21 +0100

    Finished the VideoInfoParser.

--- application/models/VideoInfoParser.php
+++ application/models/VideoInfoParser.php
@@ -1,10 +1,16 @@
 <?php
+/**
+ * Builds a Media_Item_Video_ParsedObject out of user input.
+ * Handles page urls as well as embed codes.
+ */
 class VideoInfoParser
 {
 	const DAILYMOTION_IFRAME_SRC_REGEX = '/[\S]*https?:\/\/www\.dailymotion\.com\/embed\/video\/([a-zA-Z0-9_\-]*)[\S]*/i';
-			
+	const DAILYMOTION_PAGE_URL_REGEX = '/[\S]*https?:\/\/www\.dailymotion\.com\/video\/([a-zA-Z0-9_\-]*)[\S]*/i';
+	
 	const VIMEO_IFRAME_SRC_REGEX = '/[\S]*https?:\/\/player\.vimeo\.com\/video\/([0-9]*)[\S]*/i';
-			
+	const VIMEO_PAGE_URL_REGEX = '/[\S]*https?:\/\/vimeo\.com\/([0-9]*)[\S]*/i';
+	
 	const YOUTUBE_IFRAME_SRC_REGEX = '/[\S]*https?:\/\/www\.youtube\.com\/embed\/([a-zA-Z0-9_\-]*)[\S]*/i';
 	const YOUTUBE_PAGE_URL_REGEX = '/[\S]*https?:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_\-]*)[\S]*/i';
 	const YOUTUBE_SHORT_URL_REGEX = '/[\S]*https?:\/\/youtu\.be\/([a-zA-Z0-9_\-]*)[\S]*/i';
@@ -21,41 +27,53 @@ class VideoInfoParser
 			$iframe = $iframes[0];
 			return $this->_parseIframe($iframe);
 		} else {
-			return $this->_parseUrl($input);
+			return $this->_parseUrl();
 		}
 	}
 	
+	private function _getRegexes()
+	{
+		$regexes = array();
+		$regexes[self::DAILYMOTION_IFRAME_SRC_REGEX] = Media_Item_Video::SUBTYPE_DAILYMOTION;
+		$regexes[self::DAILYMOTION_PAGE_URL_REGEX] = Media_Item_Video::SUBTYPE_DAILYMOTION;
+		
+		$regexes[self::VIMEO_IFRAME_SRC_REGEX] = Media_Item_Video::SUBTYPE_VIMEO;
+		$regexes[self::VIMEO_PAGE_URL_REGEX] = Media_Item_Video::SUBTYPE_VIMEO;
+		
+		$regexes[self::YOUTUBE_IFRAME_SRC_REGEX] = Media_Item_Video::SUBTYPE_YOUTUBE;
+		$regexes[self::YOUTUBE_PAGE_URL_REGEX] = Media_Item_Video::SUBTYPE_YOUTUBE;
+		$regexes[self::YOUTUBE_SHORT_URL_REGEX] = Media_Item_Video::SUBTYPE_YOUTUBE;
+		
+		return $regexes;
+	}
+
 	private function _parseIframe($el)
 	{
 		$src = $el->getAttribute('src');
-		$matches = null;
-
-		$matchCount = preg_match_all(self::YOUTUBE_IFRAME_SRC_REGEX, $src, $matches);
-		if($matchCount == 1){
-			$type = Media_Item_Video::SUBTYPE_YOUTUBE;
-			$id = $matches[1][0];
-			return new Media_Item_Video_ParsedObject($type, $id, $el->getAttribute('width'), $el->getAttribute('height'));
-		}
 		
-		$matchCount = preg_match_all(self::VIMEO_IFRAME_SRC_REGEX, $src, $matches);
-		if($matchCount == 1){
-			$type = Media_Item_Video::SUBTYPE_VIMEO;
-			$id = $matches[1][0];
-			return new Media_Item_Video_ParsedObject($type, $id, $el->getAttribute('width'), $el->getAttribute('height'));
+		foreach($this->_getRegexes() as $regex => $type) {
+			$matches = null;
+			$matchCount = preg_match_all($regex, $src, $matches);
+			if($matchCount == 1){
+				$id = $matches[1][0];
+				return new Media_Item_Video_ParsedObject($type, $id, $el->getAttribute('width'), $el->getAttribute('height'));
+			}
 		}
-		
-		$matchCount = preg_match_all(self::DAILYMOTION_IFRAME_SRC_REGEX, $src, $matches);
-		if($matchCount == 1){
-			$type = Media_Item_Video::SUBTYPE_DAILYMOTION;
-			$id = $matches[1][0];
-			return new Media_Item_Video_ParsedObject($type, $id, $el->getAttribute('width'), $el->getAttribute('height'));
-		}
-		
+				
 		throw new Exception("Could not parse iframe from: " . $this->_input);
 	}
 
-	private function _parseUrl($el)
+	private function _parseUrl()
 	{
-		return new Media_Item_Video_ParsedObject(null, null, 0, 0);
+		foreach($this->_getRegexes() as $regex => $type) {
+			$matches = null;
+			$matchCount = preg_match_all($regex, $this->_input, $matches);
+			if($matchCount == 1){
+				$id = $matches[1][0];
+				return new Media_Item_Video_ParsedObject($type, $id);
+			}
+		}
+		
+		throw new Exception("Could not parse: " . $this->_input);
 	}
 }
--- tests/VideoInfoParserTest.php
+++ tests/VideoInfoParserTest.php
@@ -1,6 +1,12 @@
 <?php
 require_once('ApplicationTest.php');
 
+/**
+ * Tests that the VideoInfoParser sends back the right Media_Item_Video_ParsedObject objects.
+ * 
+ * Run /usr/local/php5/bin/php /usr/local/bin/phpunit VideoInfoParserTest.php
+ * from the tests folder. 
+ */
 class VideoInfoParserTest extends ApplicationTest
 {
 	public function setup()
@@ -9,35 +15,25 @@ class VideoInfoParserTest extends ApplicationTest
 		$this->parser = new VideoInfoParser();
 	}
 	
-	private function _assertParse($input, $expected)
-	{
-		$this->assertEquals($expected, $this->parser->parse($input));
-	}
-	
-	public function _testYouTubePageUrl()
-	{
-		$input = "https://www.youtube.com/watch?v=32FB-gYr49Y";
-		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_YOUTUBE, "32FB-gYr49Y");
-		$this->_assertParse($input, $expected);
-	}
-
-	public function _testYouTubeShortenedUrl()
-	{
-		$input = "https://youtu.be/32FB-gYr49Y";
-		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_YOUTUBE, "32FB-gYr49Y");
-		$this->_assertParse($input, $expected);
-		}
-	
-	public function testYouTubeEmbedCode()
+	public function testDailyMotionIframeCode()
 	{
 		$input = <<<HTML
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/32FB-gYr49Y" frameborder="0" allowfullscreen></iframe>	
+		<iframe width="560" height="315" src="https://www.dailymotion.com/embed/video/x1buew_ouch_sport" frameborder="0" allowfullscreen></iframe>
 HTML;
-		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_YOUTUBE, "32FB-gYr49Y", 560, 315);
+		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_DAILYMOTION, "x1buew_ouch_sport", 560, 315);
 		$this->_assertParse($input, $expected);
 	}
-
-	public function testVimeoEmbedCode()
+	
+	public function testDailyMotionPageCode()
+	{
+		$input = <<<HTML
+		some bs https://www.dailymotion.com/video/x1buew_ouch_sport somebs
+HTML;
+		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_DAILYMOTION, "x1buew_ouch_sport", 480, 270);
+		$this->_assertParse($input, $expected);
+	}
+	
+	public function testVimeoIframeCode()
 	{
 		$input = <<<HTML
 		<iframe width="560" height="315" src="https://player.vimeo.com/video/140076841" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
@@ -46,15 +42,41 @@ HTML;
 		$this->_assertParse($input, $expected);
 	}
 	
-	public function testDailyMotionEmbedCode()
+	public function testVimeoPageCode()
 	{
 		$input = <<<HTML
-		<iframe width="560" height="315" src="https://www.dailymotion.com/embed/video/x1buew_ouch_sport" frameborder="0" allowfullscreen></iframe>
+		some bs https://vimeo.com/140076841 some bs
 HTML;
-		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_DAILYMOTION, "x1buew_ouch_sport", 560, 315);
+		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_VIMEO, "140076841", 640, 360);
 		$this->_assertParse($input, $expected);
 	}
+	
+	public function testYouTubeIframeCode()
+	{
+		$input = <<<HTML
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/32FB-gYr49Y" frameborder="0" allowfullscreen></iframe>
+HTML;
+		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_YOUTUBE, "32FB-gYr49Y", 560, 315);
+		$this->_assertParse($input, $expected);
+	}
+	
+	public function testYouTubePageUrl()
+	{
+		$input = "https://www.youtube.com/watch?v=32FB-gYr49Y";
+		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_YOUTUBE, "32FB-gYr49Y");
+		$this->_assertParse($input, $expected);
+	}
+	
+	public function testYouTubeShortUrl()
+	{
+		$input = "https://youtu.be/32FB-gYr49Y";
+		$expected = new Media_Item_Video_ParsedObject(Media_Item_Video::SUBTYPE_YOUTUBE, "32FB-gYr49Y");
+		$this->_assertParse($input, $expected);
+	}
+	
+	private function _assertParse($input, $expected)
+	{
+		$this->assertEquals($expected, $this->parser->parse($input));
+	}
+	
 }
-
-// /usr/local/php5/bin/php /usr/local/bin/phpunit VideoInfoParserTest.php
-// Try parsing the input as HTML. If only one text node, parse it with regexes. Otherwise try to find relevant bits.
