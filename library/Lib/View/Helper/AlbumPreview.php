73bc98c9f40647804b40a3ad474610131e73ad18
Mikael Gramont <mgramont@gmail.com>
2016-12-08 18:22:39 +0100

    Cleaned up profile page

--- library/Lib/View/Helper/AlbumPreview.php
+++ library/Lib/View/Helper/AlbumPreview.php
@@ -1,26 +1,29 @@
 <?php
 class Lib_View_Helper_AlbumPreview extends Zend_View_Helper_Abstract
 {
-	public function albumPreview(Media_Album_Row $album, $limit = 4, $doRotations = false)
+	public function albumPreview(Media_Album_Row $album, $limit = 4)
 	{
 		$medias = $this->view->albumContent($album, 1, $limit);
-		
-		$content  = '<ul class="albumPreview">'.PHP_EOL;
-		$style = "";
-		foreach($medias as $media){
-			if($doRotations){
-				$angle = rand(THUMBNAIL_ROTATION_MIN, THUMBNAIL_ROTATION_MAX);
-				$style = " style=\"-webkit-transform: rotate({$angle}deg);-moz-transform: rotate({$angle}deg);transform: rotate({$angle}deg);\"";
-			}
-			$content .= "<li$style>".$this->view->mediaThumbnail($media).'</li>'.PHP_EOL;
-		}
-		$content .= '</ul>'.PHP_EOL;
+    	$content = '<ul class="albumList albumPreview">'.PHP_EOL;
+    	foreach($medias as $media){
+    		$typeClass = $media['mediaType'] == Media_Item::TYPE_VIDEO ? 'video' : 'photo';
+    		$content .= "<li class=\"media $typeClass\">".PHP_EOL;
+    		$content .= $this->view->mediaThumbnail($media, null, null, true);		
+    		$content .= '</li>'.PHP_EOL;
+    	}
+    	$content .= '</ul>'.PHP_EOL;
+    		
 		$linkInfo = array(
 			'link' => $album->getLink(),
 			'itemType' => $album->getItemType(),
-			'name' => ucfirst(sprintf($this->view->translate('albumPreviewLink'), $album->getTitle())),
+			'name' => ucfirst(sprintf($this->view->translate(
+			        'albumPreviewLink'), $album->getTitle())),
 		);
-		$content .= '<div class="actionLinkContainer albumPreviewLink">'.$this->view->itemLink($linkInfo).'</div>'.PHP_EOL;
+		$content .= '<div class="albumPreviewLink">'.PHP_EOL;
+		$content .= ' <div class="actionLinkContainer">'.PHP_EOL;
+		$content .= $this->view->itemLink($linkInfo).PHP_EOL;
+		$content .= ' </div>'.PHP_EOL;
+		$content .= '</div>'.PHP_EOL;
 		
 		return $content;
 	}
